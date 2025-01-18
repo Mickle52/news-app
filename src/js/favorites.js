59e733f67ea3a6fc7e04d589f77eac5ef9a85e58
Mickle_PC <mickle.simakov@gmail.com>
2025-01-18 21:25:38 +0300

    0.2.0 почти готово,остался адаптив

---
+++ src/js/favorites.js
@@ -0,0 +1,94 @@
+export const testFunc = () => {
+  const foundNewsContainer = document.getElementById("found-news");
+  const favNewsContainer = document.getElementById("favorites-news");
+  const favHeadBtn = document.getElementById("favorites-button");
+  const mainContainer = document.getElementById("main");
+  const searchTest = document.getElementById("search");
+  const favoritesStatus = document.getElementById("favorites__status");
+  const foundNewsStatus = document.getElementById("found-news-status");
+
+  let favArr = [];
+  // const testGet = JSON.parse(localStorage.getItem("testHTML"));
+
+  function refreshDeletedIndex() {
+    for (let i = 0; i < favNewsContainer.children.length; i++) {
+      const deleteBtn = favNewsContainer.children[i].children[2].children[1];
+      deleteBtn.innerText = "УДАЛИТЬ";
+      deleteBtn.id = `delete-article-${i}`;
+      console.log(deleteBtn);
+    }
+  }
+
+  searchTest.addEventListener("click", (event) => {
+    if (event.target.id === "delete-alert-window") {
+      event.target.parentElement.remove();
+    }
+  });
+
+  foundNewsContainer.addEventListener("click", (event) => {
+    if (event.target.className === "news__article-button") {
+      const favArticle = event.target.parentElement.parentElement.outerHTML;
+      favArr.push(favArticle);
+      // const favArticle2 =
+      //   event.target.parentElement.parentElement.children[2].children[1];
+      // выше нахожу кнопку в избранное
+      console.log(favArr);
+      localStorage.setItem("testHTML", JSON.stringify(favArr));
+      event.target.innerText = "В ИЗБРАННОМ";
+      event.target.disabled = true;
+      event.target.style.opacity = "0.4";
+      // favNewsContainer.insertAdjacentHTML("beforeend", favArticle);
+    }
+  });
+  
+  favNewsContainer.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+      let deletedArticle = event.target.parentElement.parentElement;
+      let targetID = event.target.id;
+      let targetDeleteIndex = targetID.charAt(targetID.length - 1);
+      
+      favArr.splice(targetDeleteIndex, 1);
+      localStorage.setItem("testHTML", JSON.stringify(favArr));
+      deletedArticle.remove();
+      refreshDeletedIndex();
+      
+      if (favArr.length === 0) {
+        favoritesStatus.style.display = "flex";
+      }
+    }
+  });
+  
+  favHeadBtn.addEventListener("click", () => {
+    favNewsContainer.innerHTML = "";
+
+    const testGet = JSON.parse(localStorage.getItem("testHTML"));
+    favArr = structuredClone(testGet);
+
+    favArr.forEach((item) => {
+      favNewsContainer.insertAdjacentHTML("beforeend", item);
+    });
+
+    refreshDeletedIndex();
+
+    
+    if (favHeadBtn.innerText === "ИЗБРАННОЕ") {
+      favHeadBtn.innerText = "НАЗАД";
+      favNewsContainer.style.display = "block";
+      mainContainer.style.display = "none";
+      foundNewsStatus.style.display = "none";
+
+      if (favArr.length === 0) {
+        favoritesStatus.style.display = "flex";
+      }
+    } else if (favHeadBtn.innerText === "НАЗАД") {
+      favHeadBtn.innerText = "ИЗБРАННОЕ";
+      mainContainer.style.display = "block";
+      favNewsContainer.style.display = "none";
+      favoritesStatus.style.display = "none";
+
+      if (foundNewsContainer.children.length === 0) {
+        foundNewsStatus.style.display = "flex";
+      }
+    }
+  });
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 import "/src/css/styles.css";
+import { testFunc } from "./favorites.js";
+
+testFunc();
 
 const originUrl = "https://newsapi.org/v2/everything?";
 const apiKey = "972a777f437d4e30950e5bfc5cea08d9";
@@ -6,7 +9,8 @@ const searchNewsButton = document.getElementById("search-news-button");
 const addWordButton = document.getElementById("add-word-button");
 const avoidWordButton = document.getElementById("avoid-word-button");
 const searchWords = document.getElementById("search__words");
-const searchTest = document.getElementById("search");
+
+const foundNewsContainer = document.getElementById("found-news");
 let targetSearchWordsArray = [];
 
 async function searchNewsBySortingFilters() {
@@ -17,6 +21,7 @@ async function searchNewsBySortingFilters() {
   const dateInputTo = document.getElementById("dates-to");
   const sortingSelect = document.getElementById("sorting-select");
   const newsContainer = document.getElementById("found-news");
+  const foundNewsStatusMain = document.getElementById("found-news-status");
 
   if (targetSearchWordsArray.length === 0) {
     alertEmptyArray();
@@ -46,10 +51,19 @@ async function searchNewsBySortingFilters() {
   targetUrl += `apiKey=${apiKey}`;
   const response = await fetch(targetUrl);
   const data = await response.json();
-  console.log(data);
   console.log(response);
-  console.log(data.articles[0].author);
-
+  console.log(data);
+  // console.log(data.articles[0].author);
+  
+  if (response.ok === true && data.articles.length > 0) {
+    foundNewsStatusMain.style.display = 'none'
+  }
+  if (response.ok === true && data.articles.length === 0) {
+    foundNewsStatusMain.style.display = 'flex'
+    foundNewsStatusMain.children[0].src = './public/images/svg/nothingFound.svg'
+    foundNewsStatusMain.children[1].innerText = 'ПО ЗАДАННЫМ ПАРАМЕТРАМ НИЧЕГО НЕ НАЙДЕНО :('
+  }
+  
   data.articles.forEach((item) => {
     const publishedDate = item.publishedAt
       .replace("T", " / ")
@@ -57,17 +71,17 @@ async function searchNewsBySortingFilters() {
       .slice(0, -3);
     const article = `
     <article class="news__article">
-\t\t\t\t\t<h3 class="news__article-title">${item.title}</h3>
-\t\t\t\t\t<p class="news__article-description">${item.description}</p>
-\t\t\t\t\t<div class="news__article-buttons">
-\t\t\t\t\t\t<a href="${item.url}" class="news__article-url" target="_blank">ПЕРЕЙТИ НА ПЕРВОИСТОЧНИК</a>
-\t\t\t\t\t\t<button type="button" class="news__article-button">В ИЗБРАННОЕ</button>
-\t\t\t\t\t</div>
-\t\t\t\t\t<div class="news__article-info">
-\t\t\t\t\t\t<span class="news__article-author">Автор: ${item.author} | <span class="news__article-source">Источник: ${item.source.name}</span></span>
-\t\t\t\t\t\t<span class="news__article-publishedDate">Дата и время публикации: ${publishedDate}</span>
-\t\t\t\t\t</div>
-\t\t\t\t</article>
+     <h3 class="news__article-title">${item.title}</h3>
+     <p class="news__article-description">${item.description}</p>
+     <div class="news__article-buttons">
+      <a href="${item.url}" class="news__article-url" target="_blank">ПЕРЕЙТИ НА ПЕРВОИСТОЧНИК</a>
+      <button type="button" class="news__article-button">В ИЗБРАННОЕ</button>
+     </div>
+     <div class="news__article-info">
+      <span class="news__article-author">Автор: ${item.author} | <span class="news__article-source">Источник: ${item.source.name}</span></span>
+      <span class="news__article-publishedDate">Дата и время публикации: ${publishedDate}</span>
+     </div>
+    </article>
     `;
     newsContainer.insertAdjacentHTML("beforeend", article);
   });
@@ -83,19 +97,13 @@ function alertEmptyArray() {
   search.insertAdjacentHTML("afterbegin", phrase);
 }
 
-searchTest.addEventListener("click", (event) => {
-  if (event.target.id === "delete-alert-window") {
-    event.target.parentElement.remove();
-  }
-});
-
 searchNewsButton.addEventListener("click", () => {
+  foundNewsContainer.innerHTML = "";
   searchNewsBySortingFilters();
 });
 
 addWordButton.addEventListener("click", () => {
   const inputWord = document.getElementById("search__input");
-
   if (inputWord.value.length === 0) {
     return;
   }
